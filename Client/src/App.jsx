c7d92013101e223c27a297ef2389f5b2072731c9
Mauricio Aguero <mam.121002@gmail.com>
2024-04-06 20:04:17 -0600

    Vista: menu de curso

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,6 +7,7 @@ import RegisterUserPage from "./views/RegisterUserPage";
 import { IndexPage } from "./views/Index";
 import { HomePage } from "./views/HomePage";
 import { ViewUser } from "./views/ViewUser";
+import { MenuCurso } from "./views/MenuCurso";
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/inicio" element={<IndexPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/user" element={<ViewUser />} />
+        <Route path="/menu" element={<MenuCurso />} />
 
         {/*Catch all*/}
         <Route path="*" element={<h1>404 error</h1>} />
---
+++ Client/src/views/MenuCurso.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+
+export function MenuCurso() {
+    const classes = [
+        {
+            name: 'Clase #1',
+            lessons: [
+                {
+                    name: 'Lección #1',
+                    redirectUrl: '#',
+                },
+                {
+                    name: 'Lección #2',
+                    redirectUrl: '#',
+                },
+                {
+                    name: 'Lección #3',
+                    redirectUrl: '#',
+                },
+                {
+                    name: 'Lección #4',
+                    redirectUrl: '#',
+                },
+                {
+                    name: 'Examen #1',
+                    redirectUrl: '#',
+                },
+            ],
+        },
+        {
+            name: 'Clase #2',
+            lessons: [
+                {
+                    name: 'Lección #1',
+                    redirectUrl: '#',
+                },
+                {
+                    name: 'Lección #2',
+                    redirectUrl: '#',
+                },
+                {
+                    name: 'Lección #3',
+                    redirectUrl: '#',
+                },
+                {
+                    name: 'Lección #4',
+                    redirectUrl: '#',
+                },
+                {
+                    name: 'Examen #1',
+                    redirectUrl: '#',
+                },
+            ],
+        },
+    ]
+
+    return (
+        <div className="relative">
+            <header className="bg-red-800 text-white text-4xl font-bold text-center py-4">
+                <h1>CompuFacil</h1>
+            </header>
+
+            <div className="px-6 py-8 flex justify-center items-center flex-col">
+                {classes.map((course, index) => (
+                    <div key={index} className="mx-4 mb-8 w-full flex justify-center items-center flex-col">
+                        <h2 className="text-xl font-bold mb-4">{course.name}</h2>
+                        {course.lessons.map((lesson, lessonIndex) => (
+                                <a key={lessonIndex} href={lesson.redirectUrl} className="mb-2 inline-block w-1/3 px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-[#14453D]">
+                                    <h2 className="text-lg font-bold">{lesson.name}</h2>
+                                </a>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
