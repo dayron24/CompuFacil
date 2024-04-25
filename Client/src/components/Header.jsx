9645e371c7bee139783e980a8c532985b763dab3
DeynerNB <deynernavarrob@gmail.com>
2024-04-24 21:24:47 -0600

    Added User icon

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -2,10 +2,16 @@ import { Outlet } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <header className="bg-red-800 text-white text-4xl font-bold text-center py-4">
+    <header className="flex justify-between bg-red-800 text-white text-4xl font-bold text-center py-4">
+        <div></div>
         <a href="/home">
-        <h1>CompuFacil</h1>
+          <h1>CompuFacil</h1>
         </a>
+        <div className="mr-3">
+          <a href="/user">
+            <span className="material-icons text-4xl" style={{cursor: "pointer", userSelect: "none"}}>person</span>
+          </a>
+        </div>
     </header>
   );
 };
