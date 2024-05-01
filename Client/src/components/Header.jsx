cb3481ef4083089f36ce43c973a404a012ed220d
DeynerNB <deynernavarrob@gmail.com>
2024-05-01 17:33:19 -0600

    Responsive header

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -2,14 +2,13 @@ import { Outlet } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <header className="flex justify-between bg-red-800 text-white text-4xl font-bold text-center py-4">
-        <div></div>
-        <a href="/home">
+    <header className="grid grid-cols-2 sm:grid-cols-3 bg-red-800 text-white text-4xl font-bold text-center py-4">
+        <a href="/home" className="sm:col-start-2">
           <h1>CompuFacil</h1>
         </a>
-        <div className="mr-3">
+        <div className="text-right">
           <a href="/user">
-            <span className="material-icons text-4xl" style={{cursor: "pointer", userSelect: "none"}}>person</span>
+            <span className="material-icons text-4xl pr-3" style={{cursor: "pointer", userSelect: "none"}}>person</span>
           </a>
         </div>
     </header>
