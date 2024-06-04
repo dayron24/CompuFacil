50fa6268b733236cfbf38ef34d3324808f211ec5
Dayron <dayronpc24@gmail.com>
2024-06-04 11:38:53 -0600

    cambio de url del servidor

--- Client/src/api/axios.jsx
+++ Client/src/api/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // eslint-disable-next-line react-refresh/only-export-components
 const instance = axios.create({
-  baseURL: "https://servidorgallos.duckdns.org:446",
+  baseURL: "https://cheapserverhub.com:446",
   // baseURL: "http://localhost:8080",
   withCredentials: true,
 });
